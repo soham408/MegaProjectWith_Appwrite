438caec17fc596da455470f79e5104b4ebd7ddda
soham408 <bambadesoham408@hmail.com>
2024-12-19 13:12:23 +0530

    .env topic complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import { log } from 'console'
-import { console } from 'console';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+
   console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   
   return (
     
