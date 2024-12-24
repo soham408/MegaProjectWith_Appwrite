9553e0d5361dc953b893cf2dc18659cfa0053197
soham408 <bambadesoham408@gmail.com>
2024-12-24 14:46:35 +0530

    configure redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import react, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice' 
 import './App.css'
+import { Header } from './components'
+import { Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+const [ loading, setLoading ] = useState(true)
+const dispatch = useDispatch()
+
+useEffect(() => {
+  authService.getCurrentUser()
+   .then( (userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+   })
+   .finally(() => {
+    setLoading(false)
+   })
+})
   
-  
-  return (
-    
-    <>
-      <h1>MegaProjectWith_Appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-grey-400'>
+      <div className='bg-grey-400'>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+      </div>
+  ) : null
 }
 
 export default App
