7b891379e4d7823e4fe1f7b0a8ca83f70f01c6c1
Soham Bambade <bambadesoham408@gmail.com>
2024-12-31 06:12:39 +0000

    modified index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,19 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-// import Container from "./container/Container";
-import Container from "./container/container.jsx"
-import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Login from "./Login";
-import Button from "./Button";
-import PostForm from "./post-form/PostForm";
-import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+// import Select from "./Select"
+import Select from "../components/Select"
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container  from "../components/container/container"
+import Logo from "../components/Logo"
+import LogoutBtn from "../components/Header/LogoutBtn"
+import RTE from "../components/RTE"
+import Signup from "../components/Signup"
+import Login from "../components/Login"
+import Button from "../components/Button"
+import PostForm from "../components/post-form/PostForm"
+import PostCard from "../components/PostCard"
+import AuthLayout from "../components/AuthLayout"
+import Input from "../components/input" 
+
 export {
     Header,
     Footer,
